f318534734b890f8a4cae7471a9e756c84b1d94f
alxgeoaln <georgianalinalexandru@gmail.com>
2017-01-06 12:04:12 +0200

    angular

--- public/model/module.js
+++ public/model/module.js
@@ -1,4 +1,1 @@
 var app = angular.module('app', ['ngRoute', 'ngCookies', 'wu.masonry','ngAnimate', 'ngMaterial']);
-app.config(function ($interpolateProvider) {
-    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-});
